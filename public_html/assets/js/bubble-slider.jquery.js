6ff432e0b9bf32abf8f5f1993349cff57a0b6ee2
lmessier <leonard.messier@gmail.com>
2018-06-29 14:29:01 +0200

    feat: ajouter une configuration eslint et corriger le plugin jQuery

--- public_html/assets/js/bubble-slider.jquery.js
+++ public_html/assets/js/bubble-slider.jquery.js
@@ -1,116 +1,143 @@
+( function( $ ) {
 
-(function ($) {
+    $.fn.bubbleslider = function( options ) {
 
-    $.fn.bubbleslider = function (options) {
+        $.fn.bubbleslider.settings = $.extend( $.fn.bubbleslider.defaults, options );
 
-        $.fn.bubbleslider.settings = $.extend($.fn.bubbleslider.defaults, options);
+        return this.each( function( i, slider ) {
 
-        return this.each(function (i, slider) {
+            $.fn.bubbleslider.initialisation( $( slider ) );
+            $.fn.bubbleslider.settings.initialisation( $( slider ) );
 
-            $.fn.bubbleslider.initialisation($(slider));
-            $.fn.bubbleslider.settings.initialisation($(slider));
-            
-            $(slider).on("click", ".bubble", function () {
-                if (!$(slider).hasClass('in-progress') && !$(this).hasClass('active')) {
-                    
-                    $(slider).addClass('in-progress');
-                    
-                    $.fn.bubbleslider.settings.beforeAnimation($(slider));
+            $( slider ).on( 'click', '.bubble', function() {
+                if ( !$( slider ).hasClass( 'in-progress' ) && !$( this ).hasClass( 'active' ) ) {
 
-                    var $clickedBubble = $(this);
-                    var $firstBubble = $(slider).find('.bubble.active');
-                    var $lastBubble = $(slider).find('.bubble.last');
+                    $( slider ).addClass( 'in-progress' );
 
-                    var clickedBubbleNumber = $(this).attr('bubble-position');
-                    var lastdBubbleNumber = $lastBubble.attr('bubble-position');
+                    $.fn.bubbleslider.settings.beforeAnimation( $( slider ) );
 
-                    var clickedBubblePosition = $clickedBubble.position();
+                    var $clickedBubble = $( this );
+                    var $firstBubble = $( slider ).find( '.bubble.active' );
+                    var $lastBubble = $( slider ).find( '.bubble.last' );
+
+                    var clickedBubbleNumber = $( this ).attr( 'bubble-position' );
+                    var lastdBubbleNumber = $lastBubble.attr( 'bubble-position' );
+
                     var firstBubblePosition = $firstBubble.position();
                     var lastBubblePosition = $lastBubble.position();
 
                     var events = [];
-                    $(slider).find('.bubble').each(function (i, bubble) {
-                        if ($(bubble).attr("bubble-position") > clickedBubbleNumber) {
-                            events.push({
-                                bubble: $(bubble),
-                                position: $(slider).find('.bubble[bubble-position=' + ($(bubble).attr('bubble-position') - 1) + ']').position()
-                            });
+                    $( slider ).find( '.bubble' ).each( function( i, bubble ) {
+                        if ( $( bubble ).attr( 'bubble-position' ) > clickedBubbleNumber ) {
+                            var sel = [
+                                '.bubble[bubble-position=',
+                                ( $( bubble ).attr( 'bubble-position' ) - 1 ),
+                                ']'
+                            ].join( '' );
+                            events.push( {
+                                bubble: $( bubble ),
+                                position: $( slider ).find( sel ).position()
+                            } );
                         }
-                    });
-                                        
-                    events.forEach(function (event) {
-                        $.fn.bubbleslider.moveToPosition($(slider), event.bubble, event.position, null, function () {
-                            event.bubble.attr('bubble-position', event.bubble.attr('bubble-position') - 1);
-                            event.bubble.removeClass("last");
-                        });
-                    });
+                    } );
 
-                    $.fn.bubbleslider.moveToPosition($(slider), $clickedBubble, firstBubblePosition, clickedBubbleNumber, function () {
-                        $clickedBubble.addClass('active');
-                        $clickedBubble.removeClass("last");
-                        $clickedBubble.attr('bubble-position', 0);
-                        $.fn.bubbleslider.settings.clickedBubbleAfterAnimationCallback($clickedBubble);
-                        $(slider).removeClass('in-progress');
-                    }, 'up');
+                    events.forEach( function( event ) {
+                        $.fn.bubbleslider.moveToPosition(
+                            $( slider ),
+                            event.bubble,
+                            event.position,
+                            null,
+                            function() {
+                                event
+                                    .bubble
+                                    .attr(
+                                        'bubble-position',
+                                        event.bubble.attr( 'bubble-position' ) - 1
+                                    );
+                                event.bubble.removeClass( 'last' );
+                            } );
+                    } );
 
-                    $.fn.bubbleslider.moveToPosition($(slider), $firstBubble, lastBubblePosition, clickedBubbleNumber, function () {
-                        $firstBubble.removeClass("active");
-                        $firstBubble.addClass("last");
-                        $firstBubble.attr("bubble-position", lastdBubbleNumber);
-                        $.fn.bubbleslider.settings.firstBubbleAfterAnimationCallback($firstBubble);
-                        $(slider).removeClass('in-progress');
-                    }, 'down');
+                    $.fn.bubbleslider.moveToPosition(
+                        $( slider ),
+                        $clickedBubble,
+                        firstBubblePosition,
+                        clickedBubbleNumber,
+                        function() {
+                            $clickedBubble.addClass( 'active' );
+                            $clickedBubble.removeClass( 'last' );
+                            $clickedBubble.attr( 'bubble-position', 0 );
+                            $.fn.bubbleslider
+                                .settings
+                                .clickedBubbleAfterAnimationCallback( $clickedBubble );
+                            $( slider ).removeClass( 'in-progress' );
+                        }, 'up' );
+
+                    $.fn.bubbleslider.moveToPosition(
+                        $( slider ),
+                        $firstBubble,
+                        lastBubblePosition,
+                        clickedBubbleNumber,
+                        function() {
+                            $firstBubble.removeClass( 'active' );
+                            $firstBubble.addClass( 'last' );
+                            $firstBubble.attr( 'bubble-position', lastdBubbleNumber );
+                            $.fn.bubbleslider
+                                .settings
+                                .firstBubbleAfterAnimationCallback( $firstBubble );
+                            $( slider ).removeClass( 'in-progress' );
+                        }, 'down' );
                 }
-                    
-            });
-        });
+
+            } );
+        } );
     };
-    
+
     /**
-     * 
+     *
      */
     $.fn.bubbleslider.defaults = {
         duration: 500,
-        initialisation: function ($slider) {
-            
+        initialisation: function() {
+
         },
-        beforeAnimation: function ($slider) {
-            
+        beforeAnimation: function() {
+
         },
-        firstBubbleAfterAnimationCallback: function ($bubble) {
-            
+        firstBubbleAfterAnimationCallback: function() {
+
         },
-        clickedBubbleAfterAnimationCallback: function ($bubble) {
-            
+        clickedBubbleAfterAnimationCallback: function() {
+
         }
-    }
-    
+    };
+
     /**
      * Initialisation du slider
      * @param {type} $slider
      * @returns {undefined}
      */
-    $.fn.bubbleslider.initialisation = function ($slider) {
-        $slider.find('.bubble').first().addClass('active');
-        $slider.find('.bubble').last().addClass('last');
-        
-        $slider.css('position', 'absolute');
-        
-        $slider.find('.bubble').each(function (i, bubble) {
-            $(bubble).attr('bubble-position', i);
-            $(bubble).css({
-                top: $(bubble).offset().top - $slider.offset().top,
-                left: $(bubble).offset().left - $slider.offset().left
-            });
-        });
-        
-        $slider.find('.bubble').each(function (i, bubble) {
-            $(bubble).css({
+    $.fn.bubbleslider.initialisation = function( $slider ) {
+        $slider.find( '.bubble' ).first().addClass( 'active' );
+        $slider.find( '.bubble' ).last().addClass( 'last' );
+
+        $slider.css( 'position', 'absolute' );
+
+        $slider.find( '.bubble' ).each( function( i, bubble ) {
+            $( bubble ).attr( 'bubble-position', i );
+            $( bubble ).css( {
+                top: $( bubble ).offset().top - $slider.offset().top,
+                left: $( bubble ).offset().left - $slider.offset().left
+            } );
+        } );
+
+        $slider.find( '.bubble' ).each( function( i, bubble ) {
+            $( bubble ).css( {
                 position: 'absolute'
-            });
-        });
-    }
-    
+            } );
+        } );
+    };
+
     /**
      * Mouvement d'un element vers une position
      * @param {type} $slider
@@ -118,37 +145,49 @@
      * @param {type} $position
      * @returns {undefined}
      */
-    $.fn.bubbleslider.moveToPosition = function ($slider, $element, position, number, callback, direction) {
+    $.fn.bubbleslider.moveToPosition = function(
+        $slider,
+        $element,
+        position,
+        number,
+        callback,
+        direction ) {
 
-        if (null !== number) {
-            number = Math.pow(((number*2) - 1) / number, 1.5);
+        if ( null !== number ) {
+            number = Math.pow( ( ( number * 2 ) - 1 ) / number, 1.5 );
         }
-                
-        var baseTop = $element.css('top');
-        
-        $element.animate({
+
+        var baseTop = $element.css( 'top' );
+
+        $element.animate( {
             top: position.top,
             left: position.left
         }, {
             duration: $.fn.bubbleslider.settings.duration,
-            progress: function(animation, progress) {
-                if (null !== number) {
-                    switch (direction) {
-                        case 'up' : $element.css('top', '-=' + $.fn.bubbleslider.computeSin(progress, 75 * number)); break;
-                        case 'down' : $element.css('top', '+=' + $.fn.bubbleslider.computeSin(progress, 75 * number)); break;
+            progress: function( animation, progress ) {
+                var top;
+
+                if ( null !== number ) {
+                    switch ( direction ) {
+                    case 'up' :
+                        top = '-=' + $.fn.bubbleslider.computeSin( progress, 75 * number ); break;
+                    case 'down' :
+                        top = '+=' + $.fn.bubbleslider.computeSin( progress, 75 * number ); break;
                     }
+
+                    $element.css( 'top', top );
                 }
             },
-            complete: function () {
-                $element.css('top', baseTop);
+            complete: function() {
+                $element.css( 'top', baseTop );
                 callback();
             }
-        });
-    }
-    
-    $.fn.bubbleslider.computeSin = function (value, multiplicateur) {
-        var deg = Math.round(((180 * (value * 100)) / 100) * 100) / 100;
-        return (Math.sin((deg * Math.PI) / 180) * multiplicateur);
-    }
+        } );
+    };
 
-}(jQuery));
+    $.fn.bubbleslider.computeSin = function( value, multiplicateur ) {
+        var deg = Math.round( ( ( 180 * ( value * 100 ) ) / 100 ) * 100 ) / 100;
+        return ( Math.sin( ( deg * Math.PI ) / 180 ) * multiplicateur );
+    };
+
+} )( jQuery );
